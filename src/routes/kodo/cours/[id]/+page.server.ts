fb8828aaca1ec33635a01a4a16fbb1f9bb342e6d
PaulDelamare <paulodela2006@gmail.com>
2025-05-26 22:29:06 +0200

    ✨ Mise à jour de la fonction de chargement pour récupérer les vidéos par ID

--- src/routes/kodo/cours/[id]/+page.server.ts
+++ src/routes/kodo/cours/[id]/+page.server.ts
@@ -1,5 +1,21 @@
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import VideoApi from '$lib/Api/video.server';
 
-export const load = (async () => {
-    return {};
+export const load = (async ({ fetch, params }) => {
+    const id = params.id;
+    if (!id) {
+        throw error(404, 'Course not found');
+    }
+
+    const apiVideo = new VideoApi(fetch);
+    const response = await apiVideo.findVideoById(id);
+
+    if ("error" in response) {
+        throw error(500, "Erreur lors de la récupération de la vidéo");
+    }
+
+    return {
+        video: response.data
+    };
 }) satisfies PageServerLoad;
