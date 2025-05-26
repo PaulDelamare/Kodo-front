eebbdaf35ca7a65577350ada4e3ac97bb865ffeb
Paul_Delamare <paulodela2006@gmail.com>
2025-05-26 17:46:37 +0200

    ✨ Ajout du composant Layout pour structurer la mise en page

---
+++ src/lib/Component/layout/Layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let title: string;
+</script>
+
+<section class="">
+	<div
+		class="fixed left-0 top-10 w-full z-10 bg-surface-500 h-[50px] border-y-2 flex items-center justify-center"
+	>
+		<h2>{title}</h2>
+	</div>
+
+	<div class="flex flex-col gap-4 p-4 pt-16">
+		<slot />
+	</div>
+</section>
