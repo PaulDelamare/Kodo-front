a3581270edfb786906113e3f43e07ab42c3c65f7
PaulDelamare <paulodela2006@gmail.com>
2025-05-26 19:27:08 +0200

    ✨ Mise à jour du formulaire d'ajout de vidéo avec validation et gestion des catégories

--- src/routes/kodo/profil/add-video/+page.svelte
+++ src/routes/kodo/profil/add-video/+page.svelte
@@ -1,56 +1,75 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
+	import { goto } from '$app/navigation';
+	import Input from '$lib/Component/form/Input.svelte';
+	import Select from '$lib/Component/form/Select.svelte';
+	import SubmitButton from '$lib/Component/form/SubmitButton.svelte';
 	import VideoInput from '$lib/Component/form/VideoInput.svelte';
 	import Layout from '$lib/Component/layout/Layout.svelte';
+	import { videoSchema } from '$lib/Schema/video.schema';
 	import type { PageData } from './$types';
-	import { Autocomplete, popup } from '@skeletonlabs/skeleton';
-	import type { AutocompleteOption, PopupSettings } from '@skeletonlabs/skeleton';
+	import type { AutocompleteOption } from '@skeletonlabs/skeleton';
+	import toast from 'svelte-french-toast';
+	import { superForm } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
 
 	export let data: PageData;
 
-	let popupSettings: PopupSettings = {
-		event: 'focus-click',
-		target: 'popupAutocomplete',
-		placement: 'bottom'
-	};
+	let categorieValue = '';
 
-	let inputDemo = '';
 	const flavorOptions: AutocompleteOption<string>[] = [
-		{ label: 'Vanilla', value: 'vanilla', keywords: 'plain, basic', meta: { healthy: false } },
-		{ label: 'Chocolate', value: 'chocolate', keywords: 'dark, white', meta: { healthy: false } },
-		{ label: 'Strawberry', value: 'strawberry', keywords: 'fruit', meta: { healthy: true } },
-		{
-			label: 'Neapolitan',
-			value: 'neapolitan',
-			keywords: 'mix, strawberry, chocolate, vanilla',
-			meta: { healthy: false }
-		},
-		{ label: 'Pineapple', value: 'pineapple', keywords: 'fruit', meta: { healthy: true } },
-		{ label: 'Peach', value: 'peach', keywords: 'fruit', meta: { healthy: true } }
+		{ label: 'Graphisme', value: 'graphisme', keywords: 'graphisme, design' },
+		{ label: '3D Art', value: '3d-art', keywords: '3d, art' },
+		{ label: 'UI / UX', value: 'ui-ux', keywords: 'ui, ux' }
 	];
 
-	function onFlavorSelection(event: CustomEvent<AutocompleteOption<string>>): void {
-		inputDemo = event.detail.label;
+	let formValid = false;
+
+	const { form, errors, validateForm, message } = superForm(data.form, {
+		validators: zodClient(videoSchema),
+		validationMethod: 'oninput',
+
+		onChange: async () => {
+			const result = await validateForm();
+			if (!result.valid) {
+				formValid = false;
+				return;
+			}
+			formValid = true;
+		}
+	});
+
+	$: if ($message && $message.error) {
+		toast.error($message.error);
+	}
+
+	$: if (categorieValue) {
+		$form.categorie = categorieValue as 'graphisme' | '3d-art' | 'ui-ux';
+	}
+
+	$: if ($message && $message.success) {
+		toast.success('Votre vidéo a été publiée avec succès !');
+
+		setTimeout(() => {
+			goto($message.redirect);
+		}, 700);
+
+		$message.success = false;
 	}
 </script>
 
 <Layout title="Publication">
-	<div class="flex flex-col gap-4">
-		<VideoInput />
-
-		<div class="relative">
-			<input
-				class="w-full autocomplete !border border-tertiary-200 rounded-full"
-				type="search"
-				name="autocomplete-search"
-				bind:value={inputDemo}
-				placeholder="Search..."
-				use:popup={popupSettings}
-			/>
-			<div class="absolute bottom-0 w-full" data-popup="popupAutocomplete">
-				<div class="bg-surface-50 shadow-lg max-h-24 overflow-y-auto w-full rounded-b-md">
-					<Autocomplete bind:input={inputDemo} options={flavorOptions} />
-				</div>
-			</div>
-		</div>
-	</div>
+	<form
+		action="?/addVideo"
+		method="POST"
+		class="flex flex-col gap-4"
+		enctype="multipart/form-data"
+		use:enhance
+	>
+		<VideoInput name="video" />
+		<Select options={flavorOptions} bind:value={categorieValue} />
+		<Input name="title" placeholder="Titre de la vidéo" bind:value={$form.title} />
+		<input type="hidden" name="categorie" value={categorieValue} />
+		<SubmitButton>Publier</SubmitButton>
+	</form>
 </Layout>
