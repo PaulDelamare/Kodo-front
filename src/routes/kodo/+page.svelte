0cfe038d7477f23e4f5702df5810c3e285be2e88
Paul_Delamare <paulodela2006@gmail.com>
2025-05-26 15:00:29 +0200

    ✨ Ajout de la page Kodo avec message de connexion

---
+++ src/routes/kodo/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
+<h2>Bravo tu es connecté !</h2>
