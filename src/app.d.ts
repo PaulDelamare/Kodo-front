d0bca2ea2d706fb85f01d06247440955d7b4d053
PaulDelamare <paulodela2006@gmail.com>
2025-05-26 22:28:40 +0200

    🐛 Remplacement de la définition de l'utilisateur par le type User importé

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,14 +1,12 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { User } from "$lib/Models/user.model";
+
 // and what to do when importing types
 declare namespace App {
 	interface Locals {
-		user: {
-			id: string;
-			email: string;
-			name: string;
-			firstname: string;
-		} | null;
+		user: User | null;
 	}
 	// interface PageData {}
 	// interface Error {}
