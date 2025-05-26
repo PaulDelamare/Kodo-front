6d36558d6900e83b6a850aad1408cd040ebf6316
Paul_Delamare <paulodela2006@gmail.com>
2025-05-26 14:58:31 +0200

    ✨ Ajout de la classe UserApi pour récupérer les informations utilisateur

---
+++ src/lib/Api/user.server.ts
@@ -0,0 +1,29 @@
+import { env } from '$env/dynamic/private';
+import type { ApiResponse } from '$lib/Models/response.model';
+// import type { SuccessResponse } from '$lib/Models/api.model';
+import type { User } from '$lib/Models/user.model';
+import { Api } from './api.server';
+
+export default class UserApi extends Api {
+    getInfo = async (): Promise<ApiResponse<User & { accessToken: string }>> => {
+        try {
+            const response = await this.fetch(
+                `${env.API_URL}user/get-info`,
+                {
+                    method: 'get',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    credentials: 'include'
+                }
+            );
+
+            const data: ApiResponse<User & { accessToken: string }> = await response.json();
+            return { ...data };
+        } catch (error) {
+
+            console.error('Get info : ' + error);
+            throw new Error('Error Get info : ' + error);
+        }
+    };
+}
