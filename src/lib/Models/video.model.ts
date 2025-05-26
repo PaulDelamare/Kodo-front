e8bb97cf735e6d7a6bfe1d474ab0c9a48b7ec1ec
PaulDelamare <paulodela2006@gmail.com>
2025-05-26 19:26:45 +0200

    ✨ Ajout de l'interface Video pour modéliser les données vidéo

---
+++ src/lib/Models/video.model.ts
@@ -0,0 +1,10 @@
+export interface Video {
+     id: string;
+     title: string;
+     categorie: string;
+     videoUrl: string;
+     viewCount: number;
+     user_id: string;
+     createdAt: Date;
+     updatedAt: Date;
+}
