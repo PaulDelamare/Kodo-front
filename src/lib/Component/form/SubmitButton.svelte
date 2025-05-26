1e3ef59b6bafd9b64a9635711a621156d933d307
Paul_Delamare <paulodela2006@gmail.com>
2025-05-26 14:58:47 +0200

    ✨ Ajout du composant SubmitButton pour gérer l'envoi de formulaires

---
+++ src/lib/Component/form/SubmitButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let disabled = false;
+</script>
+
+<div class="flex justify-center">
+	<button
+		{disabled}
+		class="{disabled
+			? 'bg-gray-300 cursor-not-allowed '
+			: '!bg-secondary-500 cursor-pointer '} text-surface-500 px-4 py-2 rounded-full text-xl"
+	>
+		<slot />
+	</button>
+</div>
