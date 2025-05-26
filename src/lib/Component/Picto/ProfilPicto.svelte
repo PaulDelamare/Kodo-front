f8409cb04c1a56b3eb550927c3d9f0d1d1c3b3a6
Paul_Delamare <paulodela2006@gmail.com>
2025-05-26 14:59:19 +0200

    ✨ Ajout du composant ProfilPicto pour afficher une icône personnalisable

---
+++ src/lib/Component/Picto/ProfilPicto.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let classCustom = '';
+</script>
+
+<svg class={classCustom} viewBox="0 0 22 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+	<path
+		d="M11 11.5C13.7614 11.5 16 9.26142 16 6.5C16 3.73858 13.7614 1.5 11 1.5C8.23858 1.5 6 3.73858 6 6.5C6 9.26142 8.23858 11.5 11 11.5Z"
+		stroke-width="2"
+	/>
+	<path
+		d="M21 20.875C21 23.9812 21 26.5 11 26.5C1 26.5 1 23.9812 1 20.875C1 17.7688 5.4775 15.25 11 15.25C16.5225 15.25 21 17.7688 21 20.875Z"
+		stroke-width="2"
+	/>
+</svg>
