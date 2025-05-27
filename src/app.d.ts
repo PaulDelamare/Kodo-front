83bab20afb7c0d037bc950b273bdc64fd6ecfd83
Paul_Delamare <paulodela2006@gmail.com>
2025-05-27 10:40:01 +0200

    ✨ Mise à jour de l'interface Locals pour définir explicitement la structure de l'utilisateur

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,18 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 
-import type { User } from "$lib/Models/user.model";
 
 // and what to do when importing types
 declare namespace App {
 	interface Locals {
-		user: User | null;
+		user: {
+			id: string;
+			firstname: string;
+			name: string;
+			email: string;
+			createdAt: Date;
+			updatedAt: Date;
+		} | null;
 	}
 	// interface PageData {}
 	// interface Error {}
