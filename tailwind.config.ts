fcb55058420282e3d5f5a06503fa994d5edf07aa
PaulDelamare <paulodela2006@gmail.com>
2025-05-27 22:04:57 +0200

    Ajout de la police de caractères Modak dans la configuration de Tailwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,11 @@ export default {
 	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				modak: ['Modak', 'sans-serif'],
+			},
+		},
 	},
 	plugins: [
 		forms,
